0e0a8a0e4bb36a14b6512cb2f0e13c9b7ddd8a50
VictorMadrona <95759313+VictorMadrona@users.noreply.github.com>
2022-11-24 13:03:52 +0100

    cambio public -> protected

--- Practica 3/P3/src/Ejercicio3/comportamientoCondicional/Bandeja.java
+++ Practica 3/P3/src/Ejercicio3/comportamientoCondicional/Bandeja.java
@@ -5,32 +5,32 @@ import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 
-public class Bandeja {
+class Bandeja {
     private int capacidad;
     private List<Pieza> piezas;
 
-    public Bandeja(int capacidad) {
+    protected Bandeja(int capacidad) {
         assert(capacidad > 0);
         this.capacidad = capacidad;
         piezas = new ArrayList<>();
     }
 
-    public Enumeration<Pieza> getPiezas() {
+    protected Enumeration<Pieza> getPiezas() {
         return Collections.enumeration(piezas);
     }
 
-    public int getCapacidad() {
+    protected int getCapacidad() {
         return capacidad;
     }
 
-    public void put(Pieza p) {
+    protected void put(Pieza p) {
         if (piezas.size() < capacidad) {
             piezas.add(p);
             p.addBandeja(this);
         }
     }
 
-    public Pieza get() {
+    protected Pieza get() {
         if (piezas.size() > 0) {
             Pieza pieza = piezas.get(piezas.size() - 1);
             pieza.removeBandeja();
@@ -40,7 +40,7 @@ public class Bandeja {
         }
     }
 
-    public int size() {
+    protected int size() {
         return piezas.size();
     }
 }
